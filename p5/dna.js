bed00f4c0b5893d61a59a653447240590f329c49
zoyron <sgrarora3@gmail.com>
2020-08-20 19:00:23 +0530

    added 2nd layer of dna

--- p5/dna.js
+++ p5/dna.js
@@ -3,32 +3,72 @@ function setup() {
 }
 let t = 0;
 function draw() {
-  background("#fff");
+  background("#dadada");
   translate(width / 2, -5 * height);
+  fill("#0f0f0f");
+  // rotate(PI/4);
   stroke("#0f0f0f");
-  strokeWeight(1);
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 800; i++) {
+    strokeWeight(1.5);
+    line(xa1(t + i), ya1(t + i), xa2(t + i), ya2(t + i));
+    circle(xa1(t + i), ya1(t + i), 5);
+    circle(xa2(t + i), ya2(t + i), 5);
+    strokeWeight(1.5);
     line(x1(t + i), y1(t + i), x2(t + i) + 20, y2(t + i) + 20);
-
-    fill("#0f0f0f");
-    circle(x1(t + i), y1(t + i), 3);
-    circle(x2(t + i) + 20, y2(t + i) + 20, 3);
+    circle(x1(t + i), y1(t + i), 5);
+    circle(x2(t + i) + 20, y2(t + i) + 20, 5);
+    // strokeWeight(1.5);
+    // line(xb1(t+i),yb1(t+i),xb2(t+i),yb2(t+i));
+    // circle(xb1(t+i),yb1(t+i),5);
+    // circle(xb2(t+i),yb2(t+i),5);
   }
-  t += 0.25 / 2;
+  t += 0.25;
 }
 
 function x1(t) {
-  return sin(t / 15) * 115;
+  return sin(t / 10) * 100;
 }
 
 function y1(t) {
-  return (t / 15) * 115;
+  return (t / 10) * 100;
 }
 
 function x2(t) {
-  return -sin(t / 15) * 115;
+  return -sin(t / 10) * 100;
 }
 
 function y2(t) {
-  return (t / 15) * 115;
+  return (t / 10) * 100;
+}
+
+function xa1(t) {
+  return -cos(t / 10) * 100;
+}
+
+function ya1(t) {
+  return (t / 10) * 100;
+}
+
+function xa2(t) {
+  return cos(t / 10) * 100;
+}
+
+function ya2(t) {
+  return (t / 10) * 100;
+}
+
+function xb1(t) {
+  return -(cos(t / 10) + sin(t / 10)) * 100;
+}
+
+function yb1(t) {
+  return (t / 10) * 100;
+}
+
+function xb2(t) {
+  return (cos(t / 10) + sin(t / 10)) * 100;
+}
+
+function yb2(t) {
+  return (t / 10) * 100;
 }
