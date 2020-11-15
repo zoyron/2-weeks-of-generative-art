9d5024f34b619502e2b45583a58cb2d59faa5eba
zoyron <sgrarora3@gmail.com>
2020-11-15 20:18:29 +0530

    finished fireworks photo

---
+++ p5/fireworksPhoto.js
@@ -0,0 +1,146 @@
+let img;
+
+function preload() {
+  img = loadImage('./sanya.jpg');
+}
+
+
+function Particle(x, y, hu, firework) {
+  this.pos = createVector(x, y);
+  this.firework = firework;
+  this.lifespan = 255;
+  this.hu = hu;
+  this.acc = createVector(0, 0);
+
+  if (this.firework) {
+    this.vel = createVector(0, random(-17.5, -8));
+  } else {
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(random(2, 15));
+  }
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  };
+
+  this.update = function() {
+    if (!this.firework) {
+      this.vel.mult(0.9);
+      this.lifespan -= 2;
+    }
+
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  };
+
+  this.done = function() {
+    if (this.lifespan < 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  this.show = function() {
+    colorMode(RGB);
+
+    if (!this.firework) {
+      strokeWeight(2);
+      stroke(color(img.get(this.pos.x, this.pos.y)));
+    } else {
+      strokeWeight(6);
+      stroke(color(img.get(this.pos.x, this.pos.y)));
+    }
+
+    point(this.pos.x, this.pos.y);
+  };
+}
+
+function Firework() {
+  this.hu = random(255);
+  this.firework = new Particle(random(width), height, this.hu, true);
+  this.exploded = false;
+  this.particles = [];
+
+  this.done = function() {
+    if (this.exploded && this.particles.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  this.update = function() {
+    if (!this.exploded) {
+      this.firework.applyForce(gravity);
+      this.firework.update();
+
+      if (this.firework.vel.y >= 0) {
+        this.exploded = true;
+        this.explode();
+      }
+    }
+
+    for (var i = this.particles.length - 1; i >= 0; i--) {
+      this.particles[i].applyForce(gravity);
+      this.particles[i].update();
+
+      if (this.particles[i].done()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  };
+
+  this.explode = function() {
+    for (var i = 0; i < 200; i++) {
+      var p = new Particle(
+        this.firework.pos.x,
+        this.firework.pos.y,
+        this.hu,
+        false
+      );
+      this.particles.push(p);
+    }
+  };
+
+  this.show = function() {
+    if (!this.exploded) {
+      this.firework.show();
+    }
+
+    for (var i = 0; i < this.particles.length; i++) {
+      this.particles[i].show();
+    }
+  };
+}
+
+var fireworks = [];
+var gravity;
+
+function setup() {
+  createCanvas(img.width, img.height);
+  colorMode(RGB);
+  gravity = createVector(0, 0.2);
+  // stroke(255);
+  strokeWeight(4);
+  background(0);
+}
+
+function draw() {
+  colorMode(RGB);
+  background(0, 0,0,5);
+
+  if (random(1) < 0.1) {
+    fireworks.push(new Firework());
+  }
+
+  for (var i = fireworks.length - 1; i >= 0; i--) {
+    fireworks[i].update();
+    fireworks[i].show();
+
+    if (fireworks[i].done()) {
+      fireworks.splice(i, 1);
+    }
+  }
+}
